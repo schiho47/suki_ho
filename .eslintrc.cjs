f0411918eb764a514bed5e79f7dab44aa17ed106
schiho47 <84003532+schiho47@users.noreply.github.com>
2026-05-17 22:14:40 +0800

    2026.05.17 design token init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   extends: ['next/core-web-vitals'],
-  plugins: ['no-inline-styles', 'no-commented-code'],
+  plugins: ['no-inline-styles', 'comment-cleaner'],
   rules: {
     'no-inline-styles/no-inline-styles': 'error',
-    'no-commented-code/no-commented-code': 'error',
+    'comment-cleaner/no-commented-code': 'error',
     'prefer-const': 'error',
     eqeqeq: 'error',
     'no-console': 'warn',
     '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-img-element': 'off',
   },
   overrides: [
     {
@@ -20,7 +21,7 @@ module.exports = {
     {
       files: ['next-env.d.ts'],
       rules: {
-        'no-commented-code/no-commented-code': 'off',
+        'comment-cleaner/no-commented-code': 'off',
       },
     },
   ],
